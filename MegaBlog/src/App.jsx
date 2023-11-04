943862e7095e8a94f765084ab5585eace679f262
rishabh gehlot <rishabh3x@gmail.com>
2023-11-05 00:33:18 +0530

    Megablog commit 1

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>Code with Rishabh</h1>
+    </>
+  )
+}
+
+export default App
